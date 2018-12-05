34c49785894ec946dc3b6af94a05ef16a0420efa
Rudy <rudydimacali@gmail.com>
2018-12-04 17:17:08 -0800

    Refactored basic chaining.

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -8,30 +8,33 @@
  * HINT: We exported some similar promise-returning functions in previous exercises
  */
 
-var fs = require("fs");
-var Promise = require("bluebird");
-var promiseConstructor = require("./promiseConstructor.js");
-var promisification = require("./promisification.js");
+var fs = require('fs');
+var Promise = require('bluebird');
+var promiseConstructor = require('./promiseConstructor.js');
+var promisification = require('./promisification.js');
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
   // readFilePath and get first line
-  return promiseConstructor.pluckFirstLineFromFileAsync(readFilePath).then(
-    // send request to GitHub API for profile
-    result => {
-      promisification.getGitHubProfileAsync(result).then(result => {
-        // write JSON response to writeFilePath
-        return new Promise((resolve, reject) => {
-          fs.writeFile(writeFilePath, JSON.stringify(result), err => {
-            if (err) {
-              reject(err);
-            } else {
-              resolve();
-            }
-          });
+  return promiseConstructor
+    .pluckFirstLineFromFileAsync(readFilePath)
+    .then(
+      // send request to GitHub API for profile
+      result => {
+        return promisification.getGitHubProfileAsync(result);
+      }
+    )
+    .then(result => {
+      // write JSON response to writeFilePath
+      return new Promise((resolve, reject) => {
+        fs.writeFile(writeFilePath, JSON.stringify(result), err => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve();
+          }
         });
       });
-    }
-  );
+    });
 };
 
 // Export these functions so we can test them
